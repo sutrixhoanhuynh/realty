8578d4a065bc964f528efbf2a78c0e474bb113b6
hoan.huynh <hoan.huynh@sutrixmedia.com>
2016-12-29 17:45:05 +0700

    Add 404 page

---
+++ wp-content/themes/Avenue/404.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+<p>
+  "Oops, I screwed up and you discovered my fatal flaw.
+Well, we're not all perfect, but we try.  Can you try this
+again or maybe visit our <a
+title="Our Site" href="http://example.com/index.php">Home
+Page</a> to start fresh.  We'll do better next time."
+</p>
+<?php get_footer(); ?>
